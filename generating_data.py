d1cc4d3bde78c5b73a0a40a82e25e2571389a0e1
xiyaojin <xiyaojin@wustl.edu>
2018-11-02 15:16:00 -0500

    debug
    generating data

--- generating_data.py
+++ generating_data.py
@@ -3,29 +3,48 @@
 
 
 import numpy as np
-from os import path
+import os
 import SimpleITK as sitk
 
-img_path=r'E:\preprocessed\HGG\Brats18_2013_2_1\t1ce.nii'
-label_path=r'E:\preprocessed\HGG\Brats18_2013_2_1\truth.nii'
+X_train=[]
+Y_train=[]
 
-"""
-PixelType = itk.ctype('unsigned char')
-ImageType = itk.Image[PixelType, 3]
+def list_all_files(rootdir):
+    global X_train
+    global Y_train
+    files = []
+    list = os.listdir(rootdir) 
+    for i in range(0,len(list)):
+           path = os.path.join(rootdir,list[i])
+           if os.path.isdir(path):
+              files.extend(list_all_files(path))
+           if os.path.isfile(path):
+              if list[i]=='t1ce_hist.nii.gz':
+                  image=sitk.ReadImage(path)
+                  I_array=sitk.GetArrayFromImage(image)
+                  if X_train==[]:
+                      X_train=I_array
+                  else:
+                      X_train=np.concatenate((X_train,I_array),axis=0)
+              if list[i]=='truth.nii.gz':
+                  label=sitk.ReadImage(path)
+                  L_array=sitk.GetArrayFromImage(label)
+                  L_array[L_array!=1]=0
+                  if Y_train==[]:
+                      Y_train=L_array
+                  else:
+                      Y_train=np.concatenate((Y_train,L_array),axis=0)
+                  
+              files.append(path)
+    return files
 
-reader = itk.ImageFileReader[ImageType].New()
-reader.SetFileName(img_path)
-
-I=reader.GetOutput()
-J=I.GetPixel
-"""
-image=sitk.ReadImage(img_path)
-label=sitk.ReadImage(label_path)
-I_array=sitk.GetArrayFromImage(image)
-L_array=sitk.GetArrayFromImage(label)
-L_array[L_array!=1]=0
 
 output_image_path=r'E:\WashU-Lab\2ndRotation\x_train.npy'
 output_label_path=r'E:\WashU-Lab\2ndRotation\y_train.npy'
-np.save(output_image_path,I_array)
-np.save(output_label_path,L_array)
+#np.save(output_image_path,I_array)
+#np.save(output_label_path,L_array) 
+
+
+
+root_dir='E:\histogram_matching_results\histogram_matching_results'
+files=list_all_files(root_dir)
